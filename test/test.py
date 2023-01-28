8f489975a5b40b450dfcec493cf5dc07b40419a9
mmalyarevsky <mmalyarevsky@gmail.com>
2023-01-28 20:31:51 +0300

    added sub lt cmd test

--- test/test.py
+++ test/test.py
@@ -1,31 +1,30 @@
+from ctypes import c_int32
 import random
 import cocotb
+import numpy as np
 # import cocotb_coverage.coverage
 from cocotb.triggers import Timer
 from cocotb.result import TestSuccess
 from ialu_cmd import Scr1IaluCmdSelE
 
 ALU_WIDTH = 32
-TEST_ITER_COUNT = 5
-
-def add_model(op1, op2):
-    """model of an adder"""
-    return (op1 + op2) & ((1 << ALU_WIDTH) - 1)
-
-def sub_model(op1, op2):
-    """model of a subtractor"""
-    return (op1 - op2) & ((1 << ALU_WIDTH) - 1)
+TEST_ITER_COUNT = 100
 
 def apply_rand_in(op1, op2):
     """Set op1 and op2 to random 32 bit values"""
     op1.value = random.getrandbits(ALU_WIDTH)
     op2.value = random.getrandbits(ALU_WIDTH)
 
+def add_model(op1, op2):
+    """model of an adder"""
+    return (op1 + op2) & ((1 << ALU_WIDTH) - 1)
+
 @cocotb.test()
 async def adder_rand(dut):
     """Perform iALU add test"""
 
     log = cocotb.logging.getLogger("cocotb.test")
+    # log.setLevel('DEBUG')
 
     sim_step = Timer(1)
 
@@ -34,21 +33,25 @@ async def adder_rand(dut):
     cmd = dut.exu2ialu_cmd_i
     res = dut.ialu2exu_main_res_o
     cmd.value = Scr1IaluCmdSelE.SCR1_IALU_CMD_ADD.value
-    cmd_model = add_model
 
     for _ in range(TEST_ITER_COUNT):
         apply_rand_in(op1, op2)
         await sim_step
-        log.info('%0x + %0x = %0x', op1.value, op2.value, res.value)
-        assert res.value == cmd_model(op1.value, op2.value)
+        log.debug('%0x + %0x = %0x', op1.value, op2.value, res.value)
+        assert res.value == add_model(op1.value, op2.value)
 
     raise TestSuccess
+
+def sub_model(op1, op2):
+    """model of a subtractor"""
+    return (op1 - op2) & ((1 << ALU_WIDTH) - 1)
 
 @cocotb.test()
 async def sub_rand(dut):
     """Perform iALU subtract test"""
 
     log = cocotb.logging.getLogger("cocotb.test")
+    # log.setLevel('DEBUG')
 
     sim_step = Timer(1)
 
@@ -57,12 +60,42 @@ async def sub_rand(dut):
     cmd = dut.exu2ialu_cmd_i
     res = dut.ialu2exu_main_res_o
     cmd.value = Scr1IaluCmdSelE.SCR1_IALU_CMD_SUB.value
-    cmd_model = sub_model
 
     for _ in range(TEST_ITER_COUNT):
         apply_rand_in(op1, op2)
         await sim_step
-        log.info('%0x - %0x = %0x', op1.value, op2.value, res.value)
-        assert res.value == cmd_model(op1.value, op2.value)
+        log.debug('%0x - %0x = %0x', op1.value, op2.value, res.value)
+        assert res.value == sub_model(op1.value, op2.value)
+
+    raise TestSuccess
+
+def sub_lt_model(op1, op2):
+    """returns result of comparison: op1 < op2"""
+    cmp1 = np.array(op1.value).astype(np.int32)
+    cmp2 = np.array(op2.value).astype(np.int32)
+    if cmp1 < cmp2:
+        return 1
+    return 0
+
+@cocotb.test()
+async def sub_lt_rand(dut):
+    """Perform iALU subtract test"""
+
+    log = cocotb.logging.getLogger("cocotb.test")
+    # log.setLevel('DEBUG')
+
+    sim_step = Timer(1)
+
+    op1 = dut.exu2ialu_main_op1_i
+    op2 = dut.exu2ialu_main_op2_i
+    cmd = dut.exu2ialu_cmd_i
+    cmp_res = dut.ialu2exu_main_res_o
+    cmd.value = Scr1IaluCmdSelE.SCR1_IALU_CMD_SUB_LT.value
+
+    for _ in range(TEST_ITER_COUNT):
+        apply_rand_in(op1, op2)
+        await sim_step
+        log.debug('%0x < %0x = %0x', op1.value, op2.value, cmp_res.value)
+        assert cmp_res.value == sub_lt_model(op1.value, op2.value)
 
     raise TestSuccess
