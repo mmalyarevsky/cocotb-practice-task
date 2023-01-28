fc87a8018ef8463c96c8188cd50d7c982d0cd2b8
mmalyarevsky <mmalyarevsky@gmail.com>
2023-01-28 17:29:39 +0300

    cleanup: add test

--- test/add/test.py
+++ test/add/test.py
@@ -1,13 +1,15 @@
 import random
 import cocotb
-import cocotb_coverage.coverage
+# import cocotb_coverage.coverage
 from cocotb.triggers import Timer
-# from cocotb.clock import Clock
-from cocotb.result import TestFailure, TestSuccess
+from cocotb.result import TestSuccess
+from ialu_cmd import Scr1IaluCmdSelE
 
-def adder_model(op1: int, op2: int) -> int:
+ADDER_WIDTH = 32
+
+def add_model(op1, op2):
     """model of an adder"""
-    return op1 + op2 & 0xFFFF_FFFF
+    return (op1 + op2) & ((1 << ADDER_WIDTH) - 1)
 
 @cocotb.test()
 async def adder_rand(dut):
@@ -22,12 +24,12 @@ async def adder_rand(dut):
     cmd = dut.exu2ialu_cmd_i
     res = dut.ialu2exu_main_res_o
 
-    for _ in range(100):
-        op1.value = random.getrandbits(32)
-        op2.value = random.getrandbits(32)
-        cmd.value = 4
+    for _ in range(10000):
+        op1.value = random.getrandbits(ADDER_WIDTH)
+        op2.value = random.getrandbits(ADDER_WIDTH)
+        cmd.value = Scr1IaluCmdSelE.SCR1_IALU_CMD_ADD.value
         await sim_step
-        log.info('%x + %x = %x', op1.value, op2.value, res.value)
-        assert res.value == adder_model(op1.value, op2.value)
+        log.info('%0x + %0x = %0x', op1.value, op2.value, res.value)
+        assert res.value == add_model(op1.value, op2.value)
 
-    raise TestSuccess()
+    raise TestSuccess
